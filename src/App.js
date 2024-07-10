2b77c7c4f477702f722491bd1d70e54b2cfdd4a6
Sumit Kumar Bag <sumitkumarbag3@gmail.com>
2024-07-10 12:30:10 +0530

    UPDATE: brand img and fav icon updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from "./logo.png";
+import logo from "./assets/app/logo.png";
+import brand from "./assets/app/brand.png";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>Updated</h1>
-      <img src={logo} className="App-logo" alt="logo" />
+      <img src={logo} alt="logo" />
+      <img src={brand} alt="brand"  width={100}/>
     </div>
   );
 }
